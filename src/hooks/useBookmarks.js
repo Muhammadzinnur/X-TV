7162b37f91269f4ef46a5ccbc41c18e18a366622
Muhammadzinnur <muhammadzunurmuhammadrizo@gmail.com>
2025-11-25 14:58:25 +0500

    initial commit

--- src/hooks/useBookmarks.js
+++ src/hooks/useBookmarks.js
@@ -4,21 +4,23 @@ import { storageService } from '../utils/localStorage';
 export const useBookmarks = () => {
   const [bookmarked, setBookmarked] = useState([]);
 
-  useEffec
+  useEffect(() => {
     setBookmarked(storageService.getBookmarks());
   }, []);
 
   const toggleBookmark = (item) => {
     const exists = bookmarked.find(b => b.id === item.id);
+    
     let updated;
     if (exists) {
       updated = bookmarked.filter(b => b.id !== item.id);
     } else {
       updated = [...bookmarked, item];
     }
+
     setBookmarked(updated);
     storageService.saveBookmarks(updated);
-  };t(() => {
+  };
 
   const isBookmarked = (id) => bookmarked.some(b => b.id === id);
 
